5562cdd00b6d35a355017e79027831092769ca50
Marwan <marwan.saleh@regawa.com>
2015-04-20 13:54:16 +0700

    Update database info testing

--- engine/application/config/testing/database.php
+++ engine/application/config/testing/database.php
@@ -49,9 +49,9 @@ $active_group = 'default';
 $active_record = TRUE;
 
 $db['default']['hostname'] = 'localhost';
-$db['default']['username'] = 'k8721830_nasabahco';
+$db['default']['username'] = 'k8721830_nasabah';
 $db['default']['password'] = '7851aoyiuz';
-$db['default']['database'] = 'k8721830_nasabahco';
+$db['default']['database'] = 'k8721830_nasabah';
 $db['default']['dbdriver'] = 'mysql';
 $db['default']['dbprefix'] = 'nsc_';
 $db['default']['pconnect'] = TRUE;
